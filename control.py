0e83da3e0b3101f5993cec92925654392f315abd
Micls <cptprice123@outlook.com>
2019-11-01 17:45:04 +0700

    NEW

--- control.py
+++ control.py
@@ -1,16 +1,29 @@
 import inputs
+import pygame
 import time
 
 def main():
+    pygame.display.init()
+    pygame.joystick.init()
+    print(pygame.joystick.get_count())
+
+    controljoy=pygame.joystick.Joystick(0)
+    controljoy.init()
+    print(controljoy.get_init())
+    print(controljoy.get_id())
+    print(controljoy.get_name())
+    print(controljoy.get_numaxes())
+    print(controljoy.get_numballs())
+    print(controljoy.get_numbuttons())
+    print(controljoy.get_numhats())
+    time.sleep(3)
     while(1):
-        pads = inputs.devices.gamepads
+        pygame.event.pump()
+        x=round(controljoy.get_axis(0)*100,0)
+        y=-round(controljoy.get_axis(1)*100,0)
+        z=-round(controljoy.get_axis(2)*100,0)
+        print("X:%s,Y:%s,Z:%s" %(x,y,z))
 
-        if len(pads) == 0:
-            print("can't find gamepad")
-        else:
-            print(pads)
-
-        time.sleep(3)
 
 if __name__ == "__main__":
     main()
